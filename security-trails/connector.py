0734fa81f2d06723fdc9fb96f5abbab7984a693e
Jitesh-spryiq <jitesh.rathod@spryiq.co>
2024-01-22 14:19:14 +0530

    Addressing review comments 1. Copyright string change 2. Connector REST API Doc link . 3. Actions were tested 4. unit testing was done

--- security-trails/connector.py
+++ security-trails/connector.py
@@ -1,7 +1,6 @@
 """ Copyright start
-  Copyright (C) 2008 - 2023 Fortinet Inc.
-  All rights reserved.
-  FORTINET CONFIDENTIAL & FORTINET PROPRIETARY SOURCE CODE
+    MIT License
+    Copyright (c) 2024 Fortinet Inc
   Copyright end """
 from connectors.core.connector import Connector, get_logger, ConnectorError
 from .operations import _check_health, operations
